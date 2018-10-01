2b2c3bdb6666a0b912095f82fcd485faf178ad4b
Sérgio Pow Gomes <sergiopowgomes@MacBook-Air-de-Sergio.local>
2018-09-30 22:48:51 -0300

    PHP Crud Users
    Initial commit for php crud users importing materialize and jquery libraries.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # php-crud-user
+
 A repository in php to manage users.
+
+## MySQL
+
+```SQL
+CREATE DATABASE IF NOT EXISTS DB_PHP_CRUD_SYSTEM DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+```
+
+```SQL
+CREATE TABLE IF NOT EXISTS TB_USERS (
+    ID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Name varchar(40) NOT NULL,
+    Email varchar(50) NOT NULL UNIQUE,
+    Phone varchar(15) NOT NULL
+) DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci
+```
+
+Materialize CSS 1.0.0
+JQuery 3.3.1
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <!--Header-->
+    <nav class="indigo">
+        <div class="nav-wrapper container">
+            <div class="brand-logo">Users Register</div>
+            <ul class="right">
+                <li><a href=""><i class="material-icons left">account_circle</i>New</a></li>
+                <li><a href=""><i class="material-icons left">search</i>Users</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="materialize/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    <!--Custom JavaScript files-->
+    <script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
